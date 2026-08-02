efb172faaa3fcf8f2c851ba6d348663d08a97efc
Wilfred Alvarado <wilcr20@gmail.com>
2026-08-02 12:51:22 -0600

    Adding library to handle charts (IP)

--- src/algorithms/bubble-sort/bubble-sort.ts
+++ src/algorithms/bubble-sort/bubble-sort.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
 
+import { NgxEchartsDirective, provideEchartsCore } from 'ngx-echarts';
+// import echarts core
+import * as echarts from 'echarts/core';
+// import necessary echarts components
+import { BarChart } from 'echarts/charts';
+import { GridComponent } from 'echarts/components';
+import { CanvasRenderer } from 'echarts/renderers';
+import { EChartsCoreOption } from 'echarts/core';
+import { LineChart } from 'echarts/charts'
+
+echarts.use([BarChart, GridComponent, CanvasRenderer, LineChart]);
+
 @Component({
   selector: 'app-bubble-sort',
-  imports: [],
+  imports: [NgxEchartsDirective],
   templateUrl: './bubble-sort.html',
   styleUrl: './bubble-sort.scss',
+  providers: [provideEchartsCore({ echarts })],
 })
 export class BubbleSort {
-
+  chartOption: EChartsCoreOption = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [1, 3, 5, 8, 11, 12, 15],
+        type: 'bar',
+      },
+    ],
+  };
 }
--- src/home/home.ts
+++ src/home/home.ts
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './home.scss',
 })
 export class Home {
+  isEnglishSelected = true;
 
+  pagesUrl = [
+    {
+      label: this.isEnglishSelected ? "Bubble Sort" : "Ordenamiento de burbuja",
+      url: "bubble-sort"
+    },
+    {
+      label: this.isEnglishSelected ? "Cycle Sort" : "Ordenamiento cíclico",
+      url: "cycle-sort"
+    },
+    {
+      label: this.isEnglishSelected ? "Heap Sort" : "Ordenamiento por montículos",
+      url: "heap-sort"
+    }
+  ]
 }
